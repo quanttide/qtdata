fde4d89bf68ee568c5a83deeca9bff404d3139b9
Guo Zhang <zhangguo@quanttide.com>
2026-06-23 14:49:30 +0800

    feat: 初始化 qtdata CLI Rust 项目

---
+++ src/cli/target/debug/deps/qtdata-2eab6687ca762687.d
@@ -0,0 +1,5 @@
+/home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/target/debug/deps/qtdata-2eab6687ca762687.d: src/main.rs
+
+/home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/target/debug/deps/qtdata-2eab6687ca762687: src/main.rs
+
+src/main.rs:
---
+++ src/cli/target/debug/qtdata.d
@@ -0,0 +1,1 @@
+/home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/target/debug/qtdata: /home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/src/main.rs
