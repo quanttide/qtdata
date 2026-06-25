4be020a4939b803c5dfb04e80b31feab28bb09c0
Guo Zhang <zhangguo@quanttide.com>
2026-06-25 18:30:03 +0800

    chore: 移除 target 目录跟踪，添加 STATUS.md

--- src/cli/target/debug/deps/qtdata-2eab6687ca762687.d
+++
@@ -1,5 +0,0 @@
-/home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/target/debug/deps/qtdata-2eab6687ca762687.d: src/main.rs
-
-/home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/target/debug/deps/qtdata-2eab6687ca762687: src/main.rs
-
-src/main.rs:
--- src/cli/target/debug/qtdata.d
+++
@@ -1,1 +0,0 @@
-/home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/target/debug/qtdata: /home/iguo/repos/quanttide/default/quanttide-tech/apps/qtdata/src/cli/src/main.rs
